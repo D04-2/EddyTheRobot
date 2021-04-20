e0245dc35d91be58dcc4189b51d6b16ce4f888a1
QCzok <jan.czok@outlook.de>
2021-04-20 13:20:31 +0200

    reading input

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//place your code here
+// load instructions from the command input argument
+const instructions = process.argv[2] || "";
+// regex to allow only the letters 'G', 'L, and 'R'
+const regex = /^[GLR]*$/;
+
+if(instructions.match(regex)){
+    console.log(instructions);
+} else {
+    console.log("instructions are not valid")
+}
+
+// Eddy performs actions
+// place your code here
